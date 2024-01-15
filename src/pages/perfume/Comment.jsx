d4360c11d184b117271ff5fa152d66cca4c942a3
nanna29 <luna2907@naver.com>
2024-01-15 22:53:38 +0900

    feat: Add Comment area, Post & Cancel Btn (#4)

--- src/pages/perfume/Comment.jsx
+++ src/pages/perfume/Comment.jsx
@@ -1,43 +1,77 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 export default function Comment() {
   const [newComment, setNewComment] = useState("");
   const [comments, setComments] = useState([]);
 
-  const handleCommentChange = (e) => {
-    setNewComment(e.target.value);
+  const onChangeComment = (event) => {
+    const newComment = event.target.value;
+    setNewComment(newComment);
+    console.log(newComment);
   };
 
-  const handlePostComment = () => {
+  const PostComment = () => {
     if (newComment.trim() !== "") {
       setComments([...comments, newComment]);
       setNewComment("");
     }
   };
 
-  const handleCancelComment = () => {
+  const CancelComment = () => {
     setNewComment("");
   };
+  const Btn = styled.div`
+    margin-top: 10px;
 
+    text-align: end;
+  `;
+  const CancelBtn = styled.button`
+    background: none;
+    border: none;
+    font-size: 20px;
+    font-family: Pretendard_Medium;
+    color: #6bff94;
+  `;
+  const PostBtn = styled.button`
+    background-color: #ffffff;
+    border-radius: 30px;
+    padding: 10px 20px 10px 20px;
+    font-size: 20px;
+    font-family: Pretendard_Medium;
+    color: #7d7d7d;
+    margin-left: 30px;
+  `;
+  const CommentList = styled.div`
+    margin: 10px 0;
+    maxheight: 200px;
+    overflowy: auto;
+    background-color: #ffffff;
+  `;
   return (
-    <div>
-      <textarea
-        value={newComment}
-        onChange={handleCommentChange}
-        placeholder="댓글을 입력하세요."
-      />
-      <div>
-        <button onClick={handlePostComment}>댓글 등록</button>
-        <button onClick={handleCancelComment}>취소</button>
-      </div>
-      <div
-        className="comment-list"
-        style={{ margin: "10px 0", maxHeight: "200px", overflowY: "auto" }}
-      >
+    <>
+      <CommentList>
         {comments.map((comment, index) => (
           <div key={index}>{comment}</div>
         ))}
-      </div>
-    </div>
+      </CommentList>
+
+      <textarea
+        value={newComment}
+        onChange={onChangeComment}
+        placeholder="댓글을 입력하세요."
+        style={{
+          padding: "20px 20px 10px 20px",
+          borderRadius: "10px",
+          color: "#282727",
+          fontFamily: "Pretendard_Medium",
+          fontSize: "20px",
+        }}
+      />
+      <Btn>
+        <CancelBtn onClick={CancelComment}>취소</CancelBtn>
+        <PostBtn onClick={PostComment}>게시하기</PostBtn>
+      </Btn>
+    </>
   );
 }
