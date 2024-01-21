cc8e0a7fd5b105debb08da33f4441b69991bee6a
Gabin Kim <gabin321321@gmail.com>
2024-01-21 20:38:03 +0900

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import styled from "styled-components";
-import topImage from "../images/top_charac.png"
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import topImage from '../images/top_charac.png';
 
 const StyledContainer = styled.div`
-  display: flex; 
+  display: flex;
   align-items: center;
   justify-content: center;
   background-color: white;
@@ -26,25 +26,43 @@ const StyledContent = styled.div`
   align-items: center;
   flex-direction: column;
   background-color: white;
-`
+`;
 
 const StyledWord = styled.div`
-  margin-right: 430px;
-  font-size: 17px;
-  font-family: Pretendard_ExtraBold;
-  margin-top: 15px;
-`
+margin-right: 430px;
+font-size: 17px;
+font-family: Pretendard_ExtraBold;
+margin-top: 15px;
+text-align: left;
+`;
+
+const StyledInputContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
 
 const StyledInput = styled.input`
   width: 400px;
   height: 25px;
   padding: 7px;
   margin-top: 5px;
-  margin-right: 80px;
+  margin-right: 100px;
   border: none;
-  background-color: #f0f0f0; 
+  background-color: #f0f0f0;
   color: #333;
   border-radius: 5px;
+  font-family: Pretendard_ExtraBold;
+`;
+
+const StyledClearButton = styled.div`
+  position: absolute;
+  right: 120px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+  opacity: ${({ visible }) => (visible ? '1' : '0')};
+  transition: opacity 0.3s ease-in-out;
 `;
 
 const StyledSwitchContainer = styled.div`
@@ -79,6 +97,7 @@ const StyledSlider = styled.div`
 const StyledSwitchLabel = styled.label`
   display: flex;
   align-items: center;
+  margin-right: 30px;
 `;
 
 const StyledSwitchSpan = styled.span`
@@ -88,12 +107,12 @@ const StyledSwitchSpan = styled.span`
 `;
 
 const StyledButton = styled.button`
-  width: 420px;
+  width: 415px;
   height: 40px;
   padding: 7px;
-  margin-top: 30px; 
-  margin-bottom: 0px; 
-  margin-right: 80px; 
+  margin-top: 30px;
+  margin-bottom: 0px;
+  margin-right: 97px;
   background-color: black;
   color: white;
   border: none;
@@ -105,7 +124,7 @@ const StyledButton = styled.button`
 const StyledButtonContainer = styled.div`
   display: flex;
   margin-top: 5px;
-  margin-left: -70px;
+  margin-left: -90px;
   margin-bottom: 100px;
 `;
 
@@ -116,6 +135,7 @@ const StyledTextButton = styled.button`
   cursor: pointer;
   font-size: 10px;
   color: black;
+  font-family: Pretendard_ExtraBold;
 
   &:not(:last-child) {
     margin-right: -20px;
@@ -125,32 +145,56 @@ const StyledTextButton = styled.button`
 
 export default function Login() {
   const [isClicked, setIsClicked] = useState(false);
+  const [userid, setUserid] = useState('');
 
   const handleSwitchClick = () => {
     setIsClicked(!isClicked);
   };
 
+  const handleInputChange = (event) => {
+    setUserid(event.target.value);
+  };
+
+  const handleClearButtonClick = () => {
+    setUserid('');
+  };
+
   return (
     <>
-    <StyledContainer>
-      <StyledParagraph>
-        <p>
-          로그인하고 더 편하게<br />
-          프루스트를 이용해 보세요.
-        </p>
+      <StyledContainer>
+        <StyledParagraph>
+          <p>
+            로그인하고 더 편하게<br />
+            프루스트를 이용해 보세요.
+          </p>
         </StyledParagraph>
         <StyledImage src={topImage} alt="Top Character" width="300" height="168" />
-    </StyledContainer>
-    <StyledContent>
-      <StyledWord>
-        <p>아이디</p>
-      </StyledWord>
-      <StyledInput type="text" placeholder="아이디를 입력하세요" />
-      <StyledWord>
-        <p>비밀번호</p>
-      </StyledWord>
-      <StyledInput type="text" placeholder="비밀번호를 입력하세요" />
-      <StyledSwitchContainer>
+      </StyledContainer>
+      <StyledContent>
+        <StyledWord>
+          <p>아이디</p>
+        </StyledWord>
+        <StyledInputContainer>
+          <StyledInput
+            type="text"
+            placeholder="아이디를 입력하세요"
+            value={userid}
+            onChange={handleInputChange}
+          />
+          <StyledClearButton visible={userid !== ''} onClick={handleClearButtonClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style={{ transform: 'translateY(3.5px)' }}
+          >
+          <circle cx="12" cy="12" r="10" fill="#f0f0f0" />
+          <line x1="15" y1="9" x2="9" y2="15" />
+          <line x1="9" y1="9" x2="15" y2="15" />
+          </svg>
+          </StyledClearButton>
+        </StyledInputContainer>
+        <StyledWord>
+          <p>비밀번호</p>
+        </StyledWord>
+        <StyledInput type="text" placeholder="비밀번호를 입력하세요"/>
+        <StyledSwitchContainer>
           <StyledSwitchLabel>
             <StyledSwitch onClick={handleSwitchClick}>
               <StyledSlider isClicked={isClicked} />
@@ -165,9 +209,8 @@ export default function Login() {
           <StyledTextButton>비밀번호 찾기</StyledTextButton>
           <StyledTextButton>|</StyledTextButton>
           <StyledTextButton>회원가입 하기</StyledTextButton>
-        </StyledButtonContainer>
+          </StyledButtonContainer>
       </StyledContent>
     </>
   );
 }
-
