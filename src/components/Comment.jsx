36d6b99d7be89527d09fcfa93710720374f0c0f5
nanna29 <luna2907@naver.com>
2024-02-17 00:46:49 +0900

    feat: Add Not Logined mylist page (#34)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -92,19 +92,33 @@ export default function Comment(props) {
     const fetchData = async () => {
       try {
         if (perfumeName) {
-          const response = await axios.get(
-            `${apiUrl}/${perfumeName}/readUser`,
-            {
+          // 로그인이 되어있는 경우
+          if (localStorage.getItem("token")) {
+            const response = await axios.get(
+              `${apiUrl}/${perfumeName}/readUser`,
+              {
+                params: {
+                  Name: perfumeName,
+                },
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              }
+            );
+            console.log("댓글 전체 조회:", response.data.result);
+            setResponse(response.data.result);
+          } else {
+            const response = await axios.get(`${apiUrl}/${perfumeName}/read`, {
               params: {
                 Name: perfumeName,
               },
               headers: {
                 Authorization: `Bearer ${token}`,
               },
-            }
-          );
-          console.log("댓글 전체 조회:", response.data.result);
-          setResponse(response.data.result);
+            });
+            console.log("댓글 일부 조회:", response.data.result);
+            setResponse(response.data.result);
+          }
         } else {
           console.log("향수 이름 없음");
         }
@@ -123,40 +137,44 @@ export default function Comment(props) {
 
   const postComment = async () => {
     try {
-      if (newComment.trim() !== "") {
-        // 댓글을 서버에 POST로 전송
-        await axios.post(
-          `${apiUrl}/${perfumeName}/write`, // 앤드포인트 직접 여기에 추가
-          {
-            Content: newComment, // 서버에서 요구하는 댓글 내용에 따라 필드명을 조절해주세요.
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-              // 필요에 따라 추가적인 헤더를 여기에 포함시킬 수 있습니다.
+      if (token) {
+        if (newComment.trim() !== "") {
+          // 댓글을 서버에 POST로 전송
+          await axios.post(
+            `${apiUrl}/${perfumeName}/write`, // 앤드포인트 직접 여기에 추가
+            {
+              Content: newComment, // 서버에서 요구하는 댓글 내용에 따라 필드명을 조절해주세요.
             },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+                // 필요에 따라 추가적인 헤더를 여기에 포함시킬 수 있습니다.
+              },
+            }
+          );
+
+          // 서버 응답에 포함된 새 댓글을 사용하여 댓글 업데이트
+          setResponse((prevResponse) => ({
+            ...prevResponse,
+            perfume_comment_contents_userData: [
+              ...prevResponse.perfume_comment_contents_userData,
+              { Content: newComment },
+            ],
+          }));
+
+          // 입력창을 초기화
+          setNewComment("");
+
+          // 스크롤을 가장 하단으로 이동
+          if (commentListRef.current) {
+            commentListRef.current.scrollTop =
+              commentListRef.current.scrollHeight;
           }
-        );
-
-        // 서버 응답에 포함된 새 댓글을 사용하여 댓글 업데이트
-        setResponse((prevResponse) => ({
-          ...prevResponse,
-          perfume_comment_contents_userData: [
-            ...prevResponse.perfume_comment_contents_userData,
-            { Content: newComment },
-          ],
-        }));
-
-        // 입력창을 초기화
-        setNewComment("");
-
-        // 스크롤을 가장 하단으로 이동
-        if (commentListRef.current) {
-          commentListRef.current.scrollTop =
-            commentListRef.current.scrollHeight;
+        } else {
+          alert("댓글 내용을 작성해주세요.");
         }
       } else {
-        alert("댓글 내용을 작성해주세요.");
+        alert("로그인 후 댓글 작성이 가능합니다");
       }
     } catch (error) {
       console.error("Error posting comment:", error);
--- src/pages/perfume/MyList.jsx
+++ src/pages/perfume/MyList.jsx
@@ -105,10 +105,14 @@ const Heart = styled.div`
     margin-right: -200px;
   }
 `;
-const linkStyle = {
-  textDecoration: "none", // 밑줄 제거
-  color: "inherit", // 기본 색상 사용
-};
+
+const LoginMessage = styled.div`
+  text-align: center;
+  font-size: 50px;
+  color: white;
+  font-family: Pretendard_ExtraBold;
+  margin-top: -100px;
+`;
 
 export default function MyList() {
   const [isHeartFilled, setHeartFilled] = useState(true);
@@ -181,23 +185,25 @@ export default function MyList() {
           <img src={baseImage} alt="Base Character" />
         </BaseImage>
 
-        <Filter>
-          {/* 배열 순회하며 FilterItem 생성 */}
-          {["우디", "시트러스", "프루티", "머스크", "플로럴"].map((filter) => (
-            <FilterItem
-              key={filter} // filter 값 자체를 고유 식별자로 사용
-              // 해당 필터 배열에 존재 -> 필터 삭제
-              // 해당 필터 배열에 존재 X -> 필터 추가
-              onClick={() => onFilterClick(filter)}
-              // 필터 배열에 존재 -> true
-              // 필터 배열에 존재 X -> false
-              active={activeFilters.includes(filter)}
-            >
-              {/* 화면에 보여주는 부분 */}
-              {filter}
-            </FilterItem>
-          ))}
-        </Filter>
+        {token ? (
+          <Filter>
+            {["우디", "시트러스", "프루티", "머스크", "플로럴"].map(
+              (filter) => (
+                <FilterItem
+                  key={filter}
+                  onClick={() => onFilterClick(filter)}
+                  active={activeFilters.includes(filter)}
+                >
+                  {filter}
+                </FilterItem>
+              )
+            )}
+          </Filter>
+        ) : (
+          <LoginMessage>
+            <p>로그인 후 나만의 리스트를 확인해보세요</p>
+          </LoginMessage>
+        )}
 
         <Perfumes>
           {response &&
